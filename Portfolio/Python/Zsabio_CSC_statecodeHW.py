3e1abdeb5e28273f667c5c7bc926537e2f5bc90d
ZStuden <zen.sabio@stu.bmcc.cuny.edu>
2025-02-04 10:12:27 -0500

    Add files via upload

---
+++ Portfolio/Python/Zsabio_CSC_statecodeHW.py
@@ -0,0 +1,36 @@
+import stateCd
+
+from stateCd import abbrev_to_us_state
+
+answer = input('Do you need to look up a state code? type YES to input or NO to Cancel: ')
+
+while answer == 'YES':
+
+    state = input('Please enter 2 variable state code: ')
+
+    if state in abbrev_to_us_state:
+
+        print('The State code', state , 'is for', abbrev_to_us_state[state])
+
+    answer = input('Do you need to look up a state code? type YES to input or NO to Cancel: ')
+
+print('Bye :)')
+
+input()
+
+        
+
+             
+
+   
+
+
+
+
+
+    
+
+    
+
+
+
